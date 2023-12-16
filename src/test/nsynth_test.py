e9a7aa6bbe9dbaa3796a69e8497b0e4d4c723916
Aidan Colon <77650375+aidcol@users.noreply.github.com>
2023-12-15 21:14:19 -0600

    Add test cases

--- src/test/nsynth_test.py
+++ src/test/nsynth_test.py
@@ -5,19 +5,34 @@ from src.main.nsynth import NSynthDataset
 ROOT = "C:\\Users\\aidan\\source\\datasets\\nsynth-full"
 
 class TestNSynthDataset(unittest.TestCase):
-    def setUp(self):
-        self.train_ds = NSynthDataset(root=ROOT, subset="train")
-        self.valid_ds = NSynthDataset(root=ROOT, subset="valid")
-        self.test_ds = NSynthDataset(root=ROOT, subset="test")
-
     def test_len_train_ds(self):
-        self.assertEqual(len(self.train_ds), 289205)
+        """Verify fetching of the train split."""
+        train_ds = NSynthDataset(root=ROOT, subset='train')
+        self.assertEqual(len(train_ds), 289205)
 
     def test_len_valid_ds(self):
-        self.assertEqual(len(self.valid_ds), 12678)
+        """Verify fetching of the valid split."""
+        valid_ds = NSynthDataset(root=ROOT, subset='valid')
+        self.assertEqual(len(valid_ds), 12678)
 
     def test_len_test_ds(self):
-        self.assertEqual(len(self.test_ds), 4096)
+        """Verify fetching of the test split."""
+        test_ds = NSynthDataset(root=ROOT, subset='test')
+        self.assertEqual(len(test_ds), 4096) 
+
+    def test_choose_source(self):
+        """Verify instrument source selection for loading NSynth."""
+        instr_src = 'acoustic'
+        ds = NSynthDataset(root=ROOT, subset='test', sources=[instr_src])
+        for sample in ds:
+            self.assertEqual(sample['instrument_source_str'], instr_src) 
+    
+    def test_choose_family(self):
+        """Verify instrument family selection for loading NSynth."""
+        instr_fam = 'bass'
+        ds = NSynthDataset(root=ROOT, subset='test', families=[instr_fam])
+        for sample in ds:
+            self.assertEqual(sample['instrument_family_str'], instr_fam)
 
 
 if __name__ == '__main__':
